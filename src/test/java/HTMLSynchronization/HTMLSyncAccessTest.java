9464be9a0e438917eedef62bf052e4c52581fcea
Marco Ferreira <md.ramosf09@gmail.com>
2025-03-25 00:33:44 +0000

    unit: Implement unit testing for HTMLAsyncAccess
    - Implemented unit test for lock/unlock
    - Implemented Beforeach test a default flow
    
    Co-Authored-By: Jorge Jesus <150670017+jorgejesus03@users.noreply.github.com>

--- src/test/java/HTMLSynchronization/HTMLSyncAccessTest.java
+++ src/test/java/HTMLSynchronization/HTMLSyncAccessTest.java
@@ -1,14 +1,19 @@
 package HTMLSynchronization;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HTMLSyncAccessTest {
+    private HTMLSyncAccess htmlSyncAccess;
+    @BeforeEach
+    void setUp() {
+        htmlSyncAccess = new HTMLSyncAccess("src/test/java/HTMLSynchronization/testshtml");
+    }
 
     @Test
     void testHtmlParsingFunction() {
-        HTMLSyncAccess htmlSyncAccess = new HTMLSyncAccess("src/test/java/HTMLSynchronization/testshtml");
 
         assertNotNull(htmlSyncAccess.getSyncLockMap());
 
@@ -19,6 +24,17 @@ class HTMLSyncAccessTest {
                 () -> assertTrue(htmlSyncAccess.getSyncLockMap().containsKey("src/test/java/HTMLSynchronization/testshtml/test2/index3.html")),
                 () -> assertTrue(htmlSyncAccess.getSyncLockMap().containsKey("src/test/java/HTMLSynchronization/testshtml/index.html"))
         );
+
     }
+
+    @Test
+    void testLockUnlockByFile() {
+        htmlSyncAccess.LockFile("src/test/java/HTMLSynchronization/testshtml/index.html");
+        assertTrue(htmlSyncAccess.getSyncLockMap().get("src/test/java/HTMLSynchronization/testshtml/index.html").isLocked());
+        htmlSyncAccess.UnlockFile("src/test/java/HTMLSynchronization/testshtml/index.html");
+        assertFalse(htmlSyncAccess.getSyncLockMap().get("src/test/java/HTMLSynchronization/testshtml/test2/index3.html").isLocked());
+    }
+
+    
 
 }
